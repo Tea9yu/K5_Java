e410563b33147c7c74539ab9a2038857865d6e47
Tae9yu <ao0316@naver.com>
2023-10-19 13:46:08 +0900

    Java 공부

---
+++ src/Chap3/Test2.java
@@ -0,0 +1,11 @@
+package Chap3;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long d = 2147283648L;
+		System.out.println(d);
+	}
+
+}
